31fcf0de3cd92937595fd07b9a72529f29db7ed6
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-16 15:29:29 -0800

    Completed /auth/register endpoint.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,26 @@
 const express = require('express');
 
+const bcrypt = require("bcryptjs");
+
 const Users = require("../users/users-model.js");
 
 const router = express.Router();
+
+router.post('/register', (req, res) => {
+  console.log("test")
+  let body = req.body;
+
+  const hash = bcrypt.hashSync(body.password, 14);
+
+  body.password = hash;
+
+  Users.add(body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
 
 module.exports = router;
