b349321ae5b255c4eefdc2809b53c768784cde15
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-16 15:34:44 -0800

    Completed /auth/login endpoint except for creating session.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,8 +7,7 @@ const Users = require("../users/users-model.js");
 const router = express.Router();
 
 router.post('/register', (req, res) => {
-  console.log("test")
-  let body = req.body;
+  const body = req.body;
 
   const hash = bcrypt.hashSync(body.password, 14);
 
@@ -20,6 +19,22 @@ router.post('/register', (req, res) => {
     })
     .catch(error => {
       res.status(500).json(error);
+    })
+})
+
+router.post('/login', (req, res) => {
+  const { username, password}  = req.body;
+
+  Users.findBy({ username })
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: "Logged in!" })
+      } else {
+        res.status(401).json({ message: "You shall not pass!" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err)
     })
 })
 
