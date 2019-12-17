1756cdf8c938bb0268b8c05dc1aac28f2ae5d9bd
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-17 09:02:35 -0800

    Updated styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
-import Register from './components/Register.js'
-import Login from './components/Login.js'
+import Register from './components/Register.js';
+import Login from './components/Login.js';
+import { Menu } from 'semantic-ui-react'
 
 function App() {
+  const [ activeItem, setActiveItem ] = useState('home')
+
+  const handleItemClick = (event, {name}) => {
+    console.log(name)
+    setActiveItem(name)
+  }
+
   return (
     <div className="App">
-      <Route path="/register" component={Register}/>
-      <Route path="/login" component={Login}/>
+      <Menu tabular>
+      <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='register'
+          active={activeItem === 'register'}
+          onClick={handleItemClick}
+        />
+        <Menu.Item
+          name='login'
+          active={activeItem === 'login'}
+          onClick={handleItemClick}
+        />
+      </Menu>
+      <section class="content">
+        {activeItem === 'home' && <h1>Hello!</h1>}
+        {activeItem === 'register' && <Register />}
+        {activeItem === 'login' && <Login />}
+      </section>
     </div>
   );
 }
+
+
+
 
 export default App;
