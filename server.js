267342af9efb44e40d1fe66a89dfb18097570b19
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-17 16:42:36 -0800

    Configured api to save sessions in database.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const sessions = require('express-session');
+const KnexSessionStore = require("connect-session-knex")(sessions);
 
 const AuthRouter = require('./auth/auth-router.js');
 const UserRouter = require('./users/users-router.js');
+const knex = require("./data/db-config.js");
 
 const server = express();
 
@@ -13,6 +15,16 @@ const sessionConfiguration = {
   secret: 'keep it secret, keep it safe',
   saveUninitialized: true,
   resave: false,
+  store: new KnexSessionStore({
+    // DO NOT FORGET THE new KEYWORD
+    knex, // imported from dbConfig.js
+    createtable: true,
+
+    // optional
+    clearInterval: 1000 * 60 * 10, // defaults to 6000
+    sidfieldname: "sid",
+    tablename: "sessions",
+  }),
   cookie: {
     maxAge: 1000 * 60 * 10,
     secure: false,
