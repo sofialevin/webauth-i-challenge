c6e739576b91b0e8c3d48c61d6c137c8a13ffca8
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-17 16:38:58 -0800

    Added get users route.

--- users/users-router.js
+++ users/users-router.js
@@ -4,4 +4,14 @@ const Users = require('./users-model.js');
 
 const router = express.Router();
 
+const restricted = require('../auth/restricted-middleware.js');
+
+router.get('/', restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
 module.exports = router;
