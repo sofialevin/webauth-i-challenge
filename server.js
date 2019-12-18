f12769878525a71e9b967d9b88f2c38d3c3252ba
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-17 16:04:00 -0800

    Added session support to api.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const sessions = require('express-session');
 
 const AuthRouter = require('./auth/auth-router.js');
 const UserRouter = require('./users/users-router.js');
 
-const helmet = require('helmet');
 const server = express();
-const cors = require('cors');
+
+const sessionConfiguration = {
+  name: 'chocolatechip',
+  secret: 'keep it secret, keep it safe',
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  }
+}
 
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(sessions(sessionConfiguration));
 
 server.use('/api/auth', AuthRouter);
 server.use('/api/users', UserRouter);
